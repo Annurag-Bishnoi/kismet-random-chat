29fb5f4ca1e58eb0d66640fbdec08034ef637689
Anucdacgit <anuragbishnoi.tech@gmail.com>
2026-05-22 18:50:08 +0530

    Setup Spring Boot Backend Project

---
+++ backend/src/main/java/com/kismet/backend/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.kismet.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health(){
+        return "KisMet backend Running Successfully";
+    }
+
+
+}
